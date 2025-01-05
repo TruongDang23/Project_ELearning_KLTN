16eb1c1dd5275b67689f1cc5f7b08fee370f4068
Lê Thành Vinh <lethanhvinhspk@gmail.com>
2025-01-05 15:32:29 +0700

    cấu hình lại app error với phần test là login nhưng không có body

--- server/utils/appError.js
+++ server/utils/appError.js
@@ -1,9 +1,10 @@
 export default class AppError extends Error {
-  constructor(message, statusCode) {
-    super(message)
-    this.statusCode = statusCode
-    this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error'
-    this.isOperational = true
-    Error.captureStackTrace(this, this.constructor)
+  constructor(message, statusCode, additionalInfo = {}) {
+    super(message);
+    this.statusCode = statusCode;
+    this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+    this.isOperational = true;
+    this.additionalInfo = additionalInfo;
+    Error.captureStackTrace(this, this.constructor);
   }
 }
