be2355f1da538256caeefd33e18308d235f64840
TruongDang23 <quangtruong050123@gmail.com>
2025-03-08 12:17:25 +0700

    Test upload full course

--- server/controllers/xlsxController.js
+++ server/controllers/xlsxController.js
@@ -1,4 +1,22 @@
 import xlsx from 'xlsx'
+import fs from 'fs'
+import axios from 'axios'
+
+async function downloadFile(url, outputPath) {
+  const response = await axios({
+    url,
+    method: 'GET',
+    responseType: 'stream'
+  })
+
+  const writer = fs.createWriteStream(outputPath);
+  response.data.pipe(writer);
+
+  return new Promise((resolve, reject) => {
+    writer.on('finish', resolve)
+    writer.on('error', reject)
+  })
+}
 
 const convertToQuizObject = (workbook) => {
   const sheetName = workbook.SheetNames[0]
@@ -111,24 +129,110 @@ const convertGeneralInformation = (sheet, course_structure) => {
   return course_structure
 }
 
-const convertChapter = (sheet, course_structure, position) => {
-  const rawData = xlsx.utils.sheet_to_json(sheet)
+const convertChapter = async (sheet) => {
+  const rawData = xlsx.utils.sheet_to_json(sheet, { header: 1 })
+  let chapterObj = {}
+  let lectures = []
+  let lectureObj = {}
+  // Get chapter name
+  chapterObj.chapter_name = rawData[0][1]
 
-  return course_structure
+  // Get lecture information
+  for (let i = 2; i < rawData.length; i = i + 4) {
+    lectureObj.id = rawData[i][0]
+    lectureObj.name = rawData[i][2]
+    lectureObj.description = rawData[i+1][2]
+    lectureObj.type = rawData[i+2][2]
+    lectureObj.source = rawData[i+3][2]
+    lectureObj.QnA = []
+
+    if (lectureObj.type === 'quizz') {
+      //Download file into local disk => read file => get quizz Object => delete file
+
+      //Start downloading
+      const uniqueID = Date.now()
+      const fileUrl = lectureObj.source
+      const outputPath = `../server/uploads/${uniqueID}.xlsx`
+      await downloadFile(fileUrl, outputPath)
+        .then(() => console.log('Download completed'))
+        .catch((err) => {
+          console.error('Download failed', err)
+          return
+        })
+
+      //Start to get data from file
+      try {
+        const filePath = outputPath
+        const workbook = xlsx.readFile(filePath)
+        const quizzObject = convertToQuizObject(workbook)
+        lectureObj.passpoint = quizzObject.passpoint
+        lectureObj.during_time = quizzObject.during_time
+        lectureObj.title = quizzObject.title
+        lectureObj.questions = quizzObject.questions
+        lectureObj.type = quizzObject.type
+
+        // Delete the file using fs.unlink()
+        try {
+          fs.unlinkSync(filePath)
+        } catch (err) {
+          return
+        }
+      } catch (error) {
+        return
+      }
+    }
+    else if (lectureObj.type === 'assignment') {
+      //Download file into local disk => read file => get assignment Object => delete file
+      //Start downloading
+      const uniqueID = Date.now()
+      const fileUrl = lectureObj.source
+      const outputPath = `../server/uploads/${uniqueID}`
+      await downloadFile(fileUrl, outputPath)
+        .then(() => console.log('Download completed'))
+        .catch((err) => {
+          console.error('Download failed', err)
+          return
+        })
+
+      try {
+        const filePath = outputPath
+        const workbook = xlsx.readFile(filePath)
+        const assignmentObject = convertToAssignmentObject(workbook)
+        lectureObj.topics = assignmentObject.topics
+
+        // Delete the file using fs.unlink()
+        try {
+          fs.unlinkSync(filePath)
+        } catch (err) {
+          return
+        }
+      } catch (error) {
+        return
+      }
+    }
+    lectures.push(lectureObj)
+    lectureObj = {}
+  }
+  chapterObj.lectures = lectureObj
+  return chapterObj
 }
 
 const convertToCourseObject = (workbook) => {
   const num_sheets = workbook.SheetNames.length
   let course_structure = {}
-
+  let chapters = []
+  let chapterObj = {}
   for (let i = 1; i < num_sheets; i++) {
     const sheetName = workbook.SheetNames[i]
     const sheet = workbook.Sheets[sheetName]
     if (i == 1)
       course_structure = convertGeneralInformation(sheet, course_structure)
-    else
-      course_structure = convertChapter(sheet, course_structure, i)
+    else {
+      chapterObj = convertChapter(sheet)
+      chapters.push(chapterObj)
+    }
   }
+  course_structure.chapters = chapters
 }
 
 export { convertToQuizObject, convertToAssignmentObject, convertToCourseObject }
