4a87f650bd9cd7eaadbd260cb84b32c19ba08099
TruongDang23 <quangtruong050123@gmail.com>
2025-03-12 22:56:55 +0700

    config initial chatbot

--- client/src/screens/system/Chatbot.jsx
+++ client/src/screens/system/Chatbot.jsx
@@ -1,32 +1,45 @@
-import { useEffect } from "react";
+import { useEffect } from "react"
+
+const DOMAIN = import.meta.env.VITE_DOMAIN
+const PROJECTID = import.meta.env.VITE_PRODUCT_ID_VOICEFLOW
+const userID = localStorage.getItem("userID") ? localStorage.getItem("userID") : 'guest'
 
 const ChatBotUI = () => {
   useEffect(() => {
     if (!document.getElementById("voiceflow-chat-script")) {
-      const script = document.createElement("script");
+      const script = document.createElement("script")
       script.id = "voiceflow-chat-script"; // Thêm ID để tránh chèn nhiều lần
-      script.src = "https://cdn.voiceflow.com/widget-next/bundle.mjs";
-      script.type = "text/javascript";
-      script.async = true;
+      script.src = "https://cdn.voiceflow.com/widget-next/bundle.mjs"
+      script.type = "text/javascript"
+      script.async = true
 
       script.onload = () => {
         if (window.voiceflow && window.voiceflow.chat) {
           window.voiceflow.chat.load({
-            verify: { projectID: "67ccfa5571a708d6b598fd23" },
+            verify: { projectID: PROJECTID },
             url: "https://general-runtime.voiceflow.com",
             versionID: "production",
+            launch: {
+              event: {
+                type: "launch",
+                payload: {
+                  userID: userID,
+                  domain: DOMAIN
+                }
+              }
+            },
             voice: {
               url: "https://runtime-api.voiceflow.com"
-            },
-          });
+            }
+          })
         }
-      };
+      }
 
-      document.body.appendChild(script);
+      document.body.appendChild(script)
     }
-  }, []);
+  }, [])
 
-  return <div id="chatbot-container"></div>;
-};
+  return <div id="chatbot-container"></div>
+}
 
-export default ChatBotUI;
+export default ChatBotUI
