837b3ed678c7165892abbbb9dbd3a4ab95c33aa3
TruongDang23 <quangtruong050123@gmail.com>
2025-05-23 17:09:24 +0700

    complete test function buy course + voucher

--- server/utils/validationData.js
+++ server/utils/validationData.js
@@ -2,7 +2,7 @@ import connectMysql from '../config/connMySql.js'
 
 const connection = connectMysql.promise()
 
-async function checkEmailExists(userID, email) {
+async function checkEmailExists(email, userID) {
   const [rows] = await connection.query(
     'SELECT 1 FROM user WHERE mail = ? AND userID <> ? LIMIT 1',
     [email, userID]
